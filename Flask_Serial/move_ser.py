a8771aae0bb44d1d486162d565d0015befb3dcaa
giangnguyen <gianghnguyen84@gmail.com>
2023-10-13 12:38:52 +0700

    Flask serial

---
+++ Flask_Serial/move_ser.py
@@ -0,0 +1,34 @@
+from flask import Flask
+import serial
+import time
+
+app = Flask(__name__)
+
+ser = None
+
+@app.get("/")
+def home():
+    global ser
+    if(ser == None or ser.isOpen() == False):
+        ser = serial.Serial('COM11', 9600)
+        time.sleep(3)
+        ser.reset_input_buffer
+    return "Serial connected"
+
+@app.get("/close")
+def closeSer():
+    global ser
+    if(ser != None and ser.isOpen()):
+        ser.close()
+    return "Serial closed"
+
+
+@app.get("/move/<string:direction>")
+def move(direction):
+    global ser
+    if(ser != None and ser.isOpen()):
+        ser.write(direction.encode('utf-8'))
+    return "Move {}".format(direction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
