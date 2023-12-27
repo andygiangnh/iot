244c8fae070be4914258cf416004ecfb8bf008fd
giangnguyen <gianghnguyen84@gmail.com>
2023-12-27 11:43:45 +0700

    record lidar using joystick part 1

--- RobotCar/ML/pi_car.py
+++ RobotCar/ML/pi_car.py
@@ -1,8 +1,10 @@
 from motormodule import Motor
 from joystickmodule import get_joystick
+import record_csv as record
 
 
 motor = Motor(3, 5, 7, 15, 13, 11)
+metrics = {'turn': 0.0, 'speed': 1.0}
 while True:
     joystick = get_joystick()
     speed = 0 - joystick['hat1']
@@ -17,6 +19,12 @@ while True:
     else:
         motor.move(speed, turn, 0)
 
-    if joystick['x'] == 1:
+    metrics = {'turn': turn, 'speed': speed}
+
+    if joystick['b'] == 1:
+        record.start_record('out.txt',metrics)
+    elif joystick['a'] == 1:
+        record.stop_record()
+    elif joystick['x'] == 1:
         break
 
--- RobotCar/ML/record_csv.py
+++ RobotCar/ML/record_csv.py
@@ -1,50 +1,69 @@
 #!/usr/bin/env python3
-'''Records measurments to a given file in csv format. Usage example:
+"""Records measurements to a given file in csv format. Usage example:
 
-$ ./record_measurments.py out.txt'''
+$ ./record_csv.py out.txt"""
 import sys
 from rplidar import RPLidar
 import numpy as np
 
-
 PORT_NAME = '/dev/ttyUSB0'
+# constant based on lidar resolution
+LIDAR_RESOLUTION = 360
+
 
 def nvl(value, default):
     return value if value is not None else default
 
-def run(path):
-    '''Main function'''
+
+stop_flag = False
+
+
+def stop_record():
+    global stop_flag
+    stop_flag = True
+
+
+def start_record(path='out.txt', metrics=None):
+    global stop_flag
+    """Main function"""
+    if metrics is None:
+        metrics = {'turn': 0.0, 'speed': 1.0}
     lidar = RPLidar(PORT_NAME)
     outfile = open(path, 'w')
-    MAX_MEASUREMENT = 360
 
     try:
-        print('Recording measurments... Press Crl+C to stop.')
+        print('Recording measurements into csv format... Press Crl+C to stop.')
         skip = True
         count = 0
         line = ''
-        arr = np.empty(MAX_MEASUREMENT, dtype=object)
-        for measurment in lidar.iter_measures():
-            if measurment[0] == True:
+        arr = np.empty(LIDAR_RESOLUTION, dtype=object)
+        for measurement in lidar.iter_measures():
+            if stop_flag:
+                break
+            if measurement[0]:
                 line = ''
                 if not skip:
                     for v in arr:
-                        line += str(nvl(v,0)) + ','
-                    print(line[:-1] + '\n')
-                    outfile.write(line[:-1] + '\n')
-                
+                        line += str(nvl(v, 0)) + ','
+                    line += "{:.2f}".format(metrics['turn']) + '\n'
+                    print(line)
+                    outfile.write(line)
+
                 skip = False
-                count = 0                
-                arr = np.empty(MAX_MEASUREMENT, dtype=object)
+                count = 0
+                arr = np.empty(LIDAR_RESOLUTION, dtype=object)
             elif not skip:
-                arr[min(round(measurment[2]),359)] = measurment[3]
-                count += 1            
-            
+                arr[min(round(measurement[2]), 359)] = measurement[3]
+                count += 1
+
     except KeyboardInterrupt:
-        print('Stoping.')
-    lidar.stop()
-    lidar.disconnect()
-    outfile.close()
+        print('Stopping.')
+    finally:
+        lidar.stop()
+        lidar.disconnect()
+        outfile.close()
+        stop_flag = False
+
 
 if __name__ == '__main__':
-    run(sys.argv[1])
+    start_record(sys.argv[1])
