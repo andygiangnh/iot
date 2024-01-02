f58ea5da3d7f8bab2abf41ca98c928d816a6239e
giangnguyen <gianghnguyen84@gmail.com>
2024-01-02 15:35:16 +0700

    update algorithm for robot control module

--- RobotCar/ML/motormodule.py
+++ RobotCar/ML/motormodule.py
@@ -1,9 +1,12 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import math
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
+epsilon = 0.1
+turn_rate = math.pi * 1.3
 
 class Motor:
     def __init__(self, EnaA, In1A, In2A, EnaB, In1B, In2B):
@@ -25,33 +28,71 @@ class Motor:
         self.pwmB.start(0)
 
     def move(self, speed=0.5, turn=0.0, t=0):
-        speed *= 100
-        turn *= 100
-        leftSpeed = speed - turn
-        rightSpeed = speed + turn
+        axis1, axis2 = turn, speed
+        a = int(max(abs(axis1), abs(axis2)) * 100)
+        b = 1
+        if abs(axis2) >= epsilon:
+            b = int(math.atan(abs(axis1) / abs(axis2)) / turn_rate * a)
+        if axis2 > epsilon:
+            if axis1 < 0 - epsilon:
+                outRight = a
+                outLeft = b
+            elif axis1 > epsilon:
+                outLeft = a
+                outRight = b
+            else:
+                outLeft = int(axis2 * 100)
+                outRight = outLeft
 
-        # Limit speed between -100 and 100
-        leftSpeed = max(-100, min(100, int(leftSpeed)))
-        rightSpeed = max(-100, min(100, int(rightSpeed)))
+            self.pwmA.ChangeDutyCycle(int(outLeft))
+            self.pwmB.ChangeDutyCycle(int(outRight))
+            self.move_dir("DOWN")
+        elif axis2 < 0 - epsilon:
+            if axis1 < 0 - epsilon:
+                outRight = a
+                outLeft = b
+            elif axis1 > epsilon:
+                outLeft = a
+                outRight = b
+            else:
+                outLeft = int(abs(axis2) * 100)
+                outRight = outLeft
 
-        self.pwmA.ChangeDutyCycle(abs(leftSpeed))
-        self.pwmB.ChangeDutyCycle(abs(rightSpeed))
+            self.pwmA.ChangeDutyCycle(outLeft)
+            self.pwmB.ChangeDutyCycle(outRight)
+            self.move_dir("UP")
+        elif abs(axis2) <= epsilon:
+            outRight = int(abs(axis1) * 100)
+            outLeft = outRight
+            self.pwmA.ChangeDutyCycle(outLeft)
+            self.pwmB.ChangeDutyCycle(outRight)
 
-        if leftSpeed > 0:
-            GPIO.output(self.In1A, GPIO.HIGH)
-            GPIO.output(self.In2A, GPIO.LOW)
-        else:
-            GPIO.output(self.In1A, GPIO.LOW)
-            GPIO.output(self.In2A, GPIO.HIGH)
-
-        if rightSpeed > 0:
-            GPIO.output(self.In1B, GPIO.HIGH)
-            GPIO.output(self.In2B, GPIO.LOW)
-        else:
-            GPIO.output(self.In1B, GPIO.LOW)
-            GPIO.output(self.In2B, GPIO.HIGH)
+            if axis1 > epsilon:
+                self.move_dir("LEFT")
+            elif axis1 < 0 - epsilon:
+                self.move_dir("RIGHT")
+            else:
+                self.move_dir("STOP")
 
         sleep(t)
+
+    def move_dir(self, direction):
+        if direction == "UP":
+            self._motor_out(False, True, False, True)
+        elif direction == "DOWN":
+            self._motor_out(True, False, True, False)
+        elif direction == "LEFT":
+            self._motor_out(False, True, True, False)
+        elif direction == "RIGHT":
+            self._motor_out(True, False, False, True)
+        else:
+            self._motor_out(False, False, False, False)
+
+    def _motor_out(self, in1, in2, in3, in4):
+        GPIO.output(self.In1A, in1)
+        GPIO.output(self.In2A, in2)
+        GPIO.output(self.In1B, in3)
+        GPIO.output(self.In2B, in4)
 
     def stop(self, t=0):
         self.pwmA.ChangeDutyCycle(0)
